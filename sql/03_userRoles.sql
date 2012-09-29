639a6b7815ca45a3abfe9852283f61ab395bb1e5
chenfeng <a0092697@nus.edu.sg>
2012-09-29 23:52:57 +0800

    userrole not null

--- sql/03_userRoles.sql
+++ sql/03_userRoles.sql
@@ -1,5 +1,5 @@
 Use phoenix;
 DROP TABLE IF EXISTS `phoenix`.`user-roles`;
-CREATE  TABLE `phoenix`.`user-roles` (  `userId` VARCHAR(40) NOT NULL ,  `role` VARCHAR(45) NULL ,  PRIMARY KEY (`userId`, `role`) );
+CREATE  TABLE `phoenix`.`user-roles` (  `userId` VARCHAR(40) NOT NULL ,  `role` VARCHAR(45) NOT NULL ,  PRIMARY KEY (`userId`, `role`) );
 Insert into `phoenix`.`user-roles`
 select id, role from `phoenix`.user;
